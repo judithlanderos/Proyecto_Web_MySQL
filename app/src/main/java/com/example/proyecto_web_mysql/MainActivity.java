7451e74a75156ad8027429b80943bdce015f2fe4
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-12-04 22:40:57 -0600

    22\.Commit MainA

--- app/src/main/java/com/example/proyecto_web_mysql/MainActivity.java
+++ app/src/main/java/com/example/proyecto_web_mysql/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.proyecto_web_mysql;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import controllers.registrar_usuario;
+import controllers.validar_usuario;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +21,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        Button btnRegistro = findViewById(R.id.btnRegistro);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, registrar_usuario.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, validar_usuario.class);
+                startActivity(intent);
+            }
         });
     }
 }
