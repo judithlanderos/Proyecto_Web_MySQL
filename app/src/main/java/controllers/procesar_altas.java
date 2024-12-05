76f48c6c3df42492f5aa755fe10c39aa0fd51578
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-12-04 22:20:32 -0600

    14.Commit Android procesar altas

--- app/src/main/java/controllers/procesar_altas.java
+++ app/src/main/java/controllers/procesar_altas.java
@@ -1,4 +1,14 @@
 package controllers;
 
 public class procesar_altas {
+    public boolean procesarDatos(
+            String numControl,
+            String nombre,
+            String primerApellido,
+            String segundoApellido,
+            String fechaNacimiento,
+            String telefono,
+            String email,
+            String carrera
+    ) {
 }
