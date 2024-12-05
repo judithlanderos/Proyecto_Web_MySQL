31f95990a8cc140c77be60c0836bc21b6332bcb6
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-12-04 22:21:22 -0600

    15.Commit Android procesar altas validaciones

--- app/src/main/java/controllers/procesar_altas.java
+++ app/src/main/java/controllers/procesar_altas.java
@@ -11,4 +11,35 @@ public class procesar_altas {
             String email,
             String carrera
     ) {
+        if (!numControl.matches("^[a-zA-Z]\\d{8}$")) {
+            System.out.println("Número de control debe ser 1 letra seguida de 8 números");
+            return false;
+        }
+
+        if (!nombre.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$")) {
+            System.out.println("El nombre solo puede contener letras");
+            return false;
+        }
+
+        if (!primerApellido.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$")) {
+            System.out.println("El primer apellido solo puede contener letras");
+            return false;
+        }
+
+        if (!segundoApellido.isEmpty() && !segundoApellido.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$")) {
+            System.out.println("El segundo apellido solo puede contener letras");
+            return false;
+        }
+
+        if (!telefono.matches("^\\d{10}$")) {
+            System.out.println("El teléfono debe contener 10 dígitos");
+            return false;
+        }
+
+        if (!email.matches("^[a-zA-Z]+@[a-zA-Z]+\\.[a-zA-Z]{2,}$")) {
+            System.out.println("El correo electrónico no tiene un formato válido");
+            return false;
+        }
+
+    }
 }
